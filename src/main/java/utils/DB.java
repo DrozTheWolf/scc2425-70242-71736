647fa83fa3f1ee65105133eefc60ecf4f62f30f3
SpunkyTorch110 <torchfire110@gmail.com>
2024-11-02 21:26:09 +0000

    Redis Cache working with PostregreSQL

--- src/main/java/utils/DB.java
+++ src/main/java/utils/DB.java
@@ -3,46 +3,51 @@ package utils;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
-
+import cloudUtils.HibernateCache;
+import utils.Hibernate;
 import org.hibernate.Session;
 
 import tukano.api.Result;
 
 public class DB {
 
-	public static <T> List<T> sql(String query, Class<T> clazz) {
+	private static final boolean useCache = true;
 
-		return Hibernate.getInstance().sql(query, clazz);
+	// if false then use NoSQL
+	private static final boolean usePostegre = true;
+
+	public static <T> List<T> sql(String query, Class<T> clazz) {
+		return useCache ? HibernateCache.getInstance().sql(query, clazz) : Hibernate.getInstance().sql(query, clazz);
 	}
 	
 	public static <T> List<T> sql(Class<T> clazz, String fmt, Object ... args) {
-		return Hibernate.getInstance().sql(String.format(fmt, args), clazz);
+		return useCache ? HibernateCache.getInstance().sql(String.format(fmt, args), clazz)
+				: Hibernate.getInstance().sql(String.format(fmt, args), clazz);
 	}
 	
 	public static <T> Result<T> getOne(String id, Class<T> clazz) {
-
-		return Hibernate.getInstance().getOne(id, clazz);
+		return useCache ? HibernateCache.getInstance().getOne(id, clazz) : Hibernate.getInstance().getOne(id, clazz);
 	}
 	
 	public static <T> Result<T> deleteOne(T obj) {
-
-		return Hibernate.getInstance().deleteOne(obj);
+		return useCache ? HibernateCache.getInstance().deleteOne(obj) : Hibernate.getInstance().deleteOne(obj);
 	}
 	
 	public static <T> Result<T> updateOne(T obj) {
-
-		return Hibernate.getInstance().updateOne(obj);
+		return useCache ? HibernateCache.getInstance().updateOne(obj) : Hibernate.getInstance().updateOne(obj);
 	}
 	
 	public static <T> Result<T> insertOne( T obj) {
-		return Result.errorOrValue(Hibernate.getInstance().persistOne(obj), obj);
+		return useCache ? Result.errorOrValue(HibernateCache.getInstance().persistOne(obj), obj)
+				: Result.errorOrValue(Hibernate.getInstance().persistOne(obj), obj);
+
 	}
 	
 	public static <T> Result<T> transaction( Consumer<Session> c) {
-		return Hibernate.getInstance().execute( c::accept );
+		return useCache ? HibernateCache.getInstance().execute( c::accept ) : Hibernate.getInstance().execute( c::accept );
 	}
 	
 	public static <T> Result<T> transaction( Function<Session, Result<T>> func) {
-		return Hibernate.getInstance().execute( func );
+		return useCache ? HibernateCache.getInstance().execute( func ) : Hibernate.getInstance().execute( func );
 	}
 }
